927147c7a1b74c11b3236fd17b3308f6be5b2266
benjdaun <benjamindaunoravicius@gmail.com>
2020-11-25 12:01:49 -0500

    added more unit tests

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -15,3 +15,11 @@ export const divide = (a: number, b: number) => {
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+
+export const concat = (str1: string, str2: string) => {
+    if((str1== "") || (str2=="")) {
+        throw new Error('There\'s an empty string');
+    }
+
+    return str1 + str2;
+}
